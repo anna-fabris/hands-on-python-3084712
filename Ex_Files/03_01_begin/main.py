a9eebf46b68c8d8b75652a159b5943a6944cfba1
anna-fabris <155312679+anna-fabris@users.noreply.github.com>
2024-01-14 17:57:38 +0000

    Added comments

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -2,7 +2,9 @@ import csv
 from pprint import pprint
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
-
+# the python format for csv's would be the dictionary
+# it is really similar to a .json file
+# DictReader() is the function to read this kind of file
 EINSTEIN = {
     "birthplace": "Germany",
     "name": "Albert",
@@ -12,11 +14,11 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-with open("laureates.csv", "r") as f:
+with open("laureates.csv", "r") as f: # "r" is the read mode
     reader = csv.DictReader(f)
     laureates = list(reader)
 
 for laureate in laureates:
     if laureate["surname"] == "Einstein":
-        pprint(laureate)
+        pprint(laureate) # pretty print ^_^
         break
