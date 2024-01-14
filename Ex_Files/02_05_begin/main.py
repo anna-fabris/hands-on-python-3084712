7bf60fc51bd7cd840fbef8d0d35cf39f78114d06
anna-fabris <155312679+anna-fabris@users.noreply.github.com>
2024-01-14 17:49:50 +0000

    Added comments

--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -1,11 +1,11 @@
-import os
+import os # importing the os module
 
 DEVELOPMENT = "development"
 PRODUCTION = "production"
 STAGING = "staging"
 CODE_SPACE = "code_space"
 LOCAL = "local"
-
+# os.environ.get to access the env I am running on and grab things from there
 current_env = os.environ.get("ENV_NAME", DEVELOPMENT)
 
 if current_env == DEVELOPMENT:
@@ -14,7 +14,10 @@ elif current_env == PRODUCTION:
     print("Production environment")
 elif current_env == STAGING:
     print("Staging environment")
-elif current_env in [CODE_SPACE, LOCAL]:
+elif current_env in [CODE_SPACE, LOCAL]: # you can put a collection
     print("Codespace or local environment")
 else:
     print("Unknown environment")
+# if you 'fake' to change the env
+# f.e. export ENV_NAME="local"
+# you get a different result
