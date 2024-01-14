a4fb58354fdb686abce7936a03eb513c51499c8d
anna-fabris <155312679+anna-fabris@users.noreply.github.com>
2024-01-14 17:30:04 +0000

    Added comments

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -1,12 +1,25 @@
-greet = "Hello World"
+greet = "Hello World" 
+# string definition
+
 extened_grt = "Hello World, " + "this is a long string"
+# concatenating 2 strings
 
 name = "John"
 
 intrupution = f"Hello {name}"
+# interpolatiom of a string
 
 greet_format = "Hello {}"
+# template for formatting
 
 formatted = greet_format.format(name)
+# using the format template
 
 print(intrupution, formatted)
+
+print(formatted.upper()) # upper case
+print(formatted.lower()) # lower case
+
+print(formatted.replace("John","Paul"))
+# to replace "John" with "Paul"
+# BE CAREFUL: no replacing happens if you misspell! 
