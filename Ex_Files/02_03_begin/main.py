d76c30a8a61a64e291aadec8c78a64a84cd2e846
anna-fabris <155312679+anna-fabris@users.noreply.github.com>
2024-01-14 17:35:08 +0000

    Added comments

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -1,13 +1,17 @@
+# This code is about LISTS:
 NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
+# the slice [:] can be used also for strings
 JOHN_PAUL = NAMES[:2]
 GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
+REVERSE = NAMES[::-1] 
+# reverse: start - stop - step
 EVERY_OTHER = NAMES[::2]
+# every other: just the step
 
 print(sum(AGES))
 print(min(AGES))
